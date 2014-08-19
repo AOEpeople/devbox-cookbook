6118a9e727ffbad4e576140754b2c2ca057936ff
Fabrizio Branca <fabrizio.branca@aoe.com>
2014-08-19 16:24:29 +0000

    Added samba configuration

--- recipes/samba.rb
+++ recipes/samba.rb
@@ -6,7 +6,7 @@ execute "Create samba user for #{node[:devbox][:main_user]}" do
   command "echo '#{node[:devbox][:main_user]}\n#{node[:devbox][:main_user]}' | smbpasswd -s -a #{node[:devbox][:main_user]}"
 end
 
-template "Samba configuration" do
+template "/etc/samba/smb.conf" do
   source 'smb.conf.erb'
   mode 00644
   owner 'root'
@@ -16,4 +16,11 @@ template "Samba configuration" do
   variables(
     valid_users: node[:devbox][:main_user]
   )
+  notifies :restart, 'service[smbd]', :delayed
+end
+
+service 'smbd' do
+  pattern 'smbd'
+  supports [:restart, :status]
+  action [:enable, :start]
 end
