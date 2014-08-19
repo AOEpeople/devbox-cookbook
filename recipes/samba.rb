bfa97ef6e3f2c90c82fa064c567e21f864dbf665
Fabrizio Branca <fabrizio.branca@aoe.com>
2014-08-19 16:01:51 +0000

    Samba configuration

--- recipes/samba.rb
+++ recipes/samba.rb
@@ -6,4 +6,14 @@ execute "Create samba user for #{node[:devbox][:main_user]}" do
   command "echo '#{node[:devbox][:main_user]}\n#{node[:devbox][:main_user]}' | smbpasswd -s -a #{node[:devbox][:main_user]}"
 end
 
-
+template "Samba configuration" do
+  source 'smb.conf.erb'
+  mode 00644
+  owner 'root'
+  group 'root'
+  cookbook 'devbox'
+  action :create
+  variables(
+    valid_users: node[:devbox][:main_user]
+  )
+end
