b496228b8d0962d569d49d67834392084acf3af4
Fabrizio Branca <fabrizio.branca@aoe.com>
2014-11-06 01:01:18 +0000

    Added support for motd and hosts files (wip)

--- recipes/instances.rb
+++ recipes/instances.rb
@@ -8,6 +8,13 @@ group 'systemstorage' do
   action :create
 end
 
+ip=`ifconfig eth1 | grep "inet " | awk -F'[: ]+' '{ print $4 }'`.strip
+
+local_hostsfile = ""
+hosts_hostsfile = ""
+
+motd = ""
+
 sites = data_bag(node['devbox']['magento_instances_databag_name'])
 sites.each do |site|
   opts = data_bag_item(node['devbox']['magento_instances_databag_name'], site)
@@ -43,4 +50,30 @@ sites.each do |site|
     end
   end
 
+  local_hostsfile << "127.0.0.1    #{opts["server_name"]}"
+  hosts_hostsfile << "#{ip}    #{opts["server_name"]}"
+  opts["server_aliases"].each do |h|
+    local_hostsfile << " #{h}"
+    hosts_hostsfile << " #{h}"
+  end
+  local_hostsfile << "\n"
+  hosts_hostsfile << "\n"
+
+  opts["motd"].each do |line|
+    motd << "#{line}\n"
+  end
+
 end
+
+file '/etc/motd' do
+  content motd
+end
+
+file '/etc/hosts.local' do
+  content local_hostsfile
+end
+
+file '/var/www/hosts.hosts' do
+  content hosts_hostsfile
+end
+
