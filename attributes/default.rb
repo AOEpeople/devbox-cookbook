43979ade710873bc95b8cdc8e1dcaa429a29eb39
Fabrizio Branca <github@fabrizio-branca.de>
2014-12-15 13:34:55 -0800

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,7 +13,7 @@ default['apache']['version'] = '2.4'
 normal['apache']['version'] = '2.4'
 override['apache']['version'] = '2.4'
 
-include_attribute 'mysql'
+#include_attribute 'mysql'
 default['mysql']['server_debian_password'] = 'root'
 default['mysql']['server_root_password'] = 'root'
 default['mysql']['server_repl_password'] = 'root'
