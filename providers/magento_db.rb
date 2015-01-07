de8a5d0fa92ea76f98fe068f3f8732c93dbd0dfe
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-01-07 18:21:44 +0000

    Double slashes

--- providers/magento_db.rb
+++ providers/magento_db.rb
@@ -4,11 +4,11 @@ action :create do
      password = new_resource.password || new_resource.name
 
      execute "Create database user #{name}" do
-       command "mysql -u root -e 'CREATE USER \'#{name}\'@\'localhost\' IDENTIFIED BY \'#{password}\';'"
+       command "mysql -u root -e 'CREATE USER \\'#{name}\\'@\\'localhost\\' IDENTIFIED BY \\'#{password}\\';'"
      end
 
      execute "Grant usage to #{name}" do
-       command "mysql -u root -e 'GRANT USAGE ON *.* TO \'#{name}\'@\'localhost\' IDENTIFIED BY \'#{password}\' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;'"
+       command "mysql -u root -e 'GRANT USAGE ON *.* TO \\'#{name}\\'@\\'localhost\\' IDENTIFIED BY \\'#{password}\\' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;'"
      end
 
      execute "Create database #{name}" do
@@ -16,7 +16,7 @@ action :create do
      end
 
      execute "Grant privileges #{name}" do
-       command "mysql -u root -e 'GRANT ALL PRIVILEGES ON \'#{name}\'.* TO '#{name}'@'localhost';'"
+       command "mysql -u root -e 'GRANT ALL PRIVILEGES ON \\'#{name}\\'.* TO '#{name}'@'localhost';'"
      end
 
      execute "Flush privileges" do
