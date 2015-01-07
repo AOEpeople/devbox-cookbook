f3752b690d0141c8680a939865a1f9a764ba2cb1
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-01-07 18:27:27 +0000

    Double slashes

--- providers/magento_db.rb
+++ providers/magento_db.rb
@@ -1,26 +1,26 @@
 action :create do
 
-     name = new_resource.name
-     password = new_resource.password || new_resource.name
+  name = new_resource.name
+  password = new_resource.password || new_resource.name
 
-     execute "Create database user #{name}" do
-       command "mysql -u root -e \"CREATE USER '#{name}'@'localhost' IDENTIFIED BY '#{password}';\""
-     end
+  execute "Create database user #{name}" do
+    command "mysql -u root -e \"CREATE USER '#{name}'@'localhost' IDENTIFIED BY '#{password}';\""
+  end
 
-     execute "Grant usage to #{name}" do
-       command "mysql -u root -e \"GRANT USAGE ON *.* TO '#{name}'@'localhost' IDENTIFIED BY '#{password}' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;\""
-     end
+  execute "Grant usage to #{name}" do
+    command "mysql -u root -e \"GRANT USAGE ON *.* TO '#{name}'@'localhost' IDENTIFIED BY '#{password}' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;\""
+  end
 
-     execute "Create database #{name}" do
-       command "mysql -u root -e \"CREATE DATABASE IF NOT EXISTS #{name};\""
-     end
+  execute "Create database #{name}" do
+    command "mysql -u root -e \"CREATE DATABASE IF NOT EXISTS #{name};\""
+  end
 
-     execute "Grant privileges #{name}" do
-       command "mysql -u root -e \"GRANT ALL PRIVILEGES ON '#{name}'.* TO '#{name}'@'localhost';\""
-     end
+  execute "Grant privileges #{name}" do
+    command "mysql -u root -e \"GRANT ALL PRIVILEGES ON `#{name}`.* TO '#{name}'@'localhost';\""
+  end
 
-     execute "Flush privileges" do
-       command "mysql -u root -e \"FLUSH PRIVILEGES;\""
-     end
+  execute "Flush privileges" do
+    command "mysql -u root -e \"FLUSH PRIVILEGES;\""
+  end
 
-   end
+end
