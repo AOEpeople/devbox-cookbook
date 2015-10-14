905bdd0f5935e0e8b08ea823aa4d842e767b4c0d
Fabrizio Branca <github@fabrizio-branca.de>
2015-10-14 14:23:09 -0700

    Adding php5-xsl (required for Magento 2)

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,7 @@ default['devbox']['packages'] = [
    'unzip', 'graphicsmagick', 'jq', 'tig', 'git', 'pv', 'realpath',
    'apache2',
    'mysql-server-5.6', 'mysql-client-5.6',
-   'php5', 'php5-curl', 'php5-gd', 'php5-mcrypt', 'php5-redis', 'php5-mhash', 'php5-cli', 'php5-mysql', 'php5-gd', 'php5-intl', 'php5-common'
+   'php5', 'php5-curl', 'php5-gd', 'php5-mcrypt', 'php5-redis', 'php5-mhash', 'php5-cli', 'php5-mysql', 'php5-gd', 'php5-intl', 'php5-common', 'php5-xsl'
 ]
 
 default['devbox']['apache_modules'] = [ 'headers', 'rewrite' ]
